3a3fe1972e9f442afa1fd89fe5d737688e69f859
Roy Anger <roy@royanger.com>
2021-05-30 20:06:30 -0400

    Started fleshing out Footer, added social icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'
 import type { AppProps } from 'next/app'
 import '../styles/styles.scss'
+import Image from 'next/image'
+
+import Website from '../images/home-lg-alt.svg'
+import Twitter from '../images/twitter.svg'
+import GitHub from '../images/github.svg'
+import NPM from '../images/npm.svg'
 
 interface modalPositionProps {
    (x: number, y: number): void
@@ -36,7 +42,41 @@ function MyApp({ Component, pageProps }: AppProps) {
             {...pageProps}
          />
          <footer>
-            <p>by Roy Anger Welcome</p>
+            <div className="container">
+               <p>by Roy Anger</p>
+               <p>
+                  Both the NPM package and this website are released under the{' '}
+                  <a href="https://mit-license.org/">MIT License</a>
+               </p>
+
+               <div className="socials">
+                  <p>See my socials and work:</p>
+                  <a href="https://royanger.com">
+                     <Website
+                        width="40px"
+                        height="40px"
+                        fill="var(--light-text)"
+                     />
+                  </a>
+                  <a href="https://twitter.com/royanger">
+                     <Twitter
+                        width="40px"
+                        height="40px"
+                        fill="var(--light-text)"
+                     />
+                  </a>
+                  <a href="https://github.com/royanger">
+                     <GitHub
+                        width="40px"
+                        height="40px"
+                        fill="var(--light-text)"
+                     />
+                  </a>
+                  <a href="https://www.npmjs.com/~royanger">
+                     <NPM width="40px" height="40px" fill="var(--light-text)" />
+                  </a>
+               </div>
+            </div>
          </footer>
          <div id="modal">
             <div
